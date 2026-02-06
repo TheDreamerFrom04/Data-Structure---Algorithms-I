c25ed27d8a27fe208b52b3fd862418e36e68fca6
TheDreamerFrom04 <110796962+TheDreamerFrom04@users.noreply.github.com>
2026-02-05 19:09:44 -0500

    Delete Charmander directory

--- Charmander/Battle.java
+++
@@ -1,8 +0,0 @@
-public class Battle
-{
-    public static void main(String[] args) {
-        Squirtle player1 = new Squirtle("Gary");
-        Charmander player2 = new Charmander("Ash");
-        
-    }
-}
